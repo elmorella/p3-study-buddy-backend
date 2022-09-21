f36d4f9e68169ddbd4d494d95f9c9bd76345ecfe
Elmer Orellana <104410892+ssblue18@users.noreply.github.com>
2022-09-21 14:00:08 -0700

    initial commit

---
+++ src/main/java/com/revature/studybuddy/StudyBuddyApplication.java
@@ -0,0 +1,13 @@
+package com.revature.studybuddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyBuddyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyBuddyApplication.class, args);
+	}
+
+}
