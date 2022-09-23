a2e00010d8f37b512664793bf4f5b4acb37a5fb2
dMeighan <93158202+dMeighan@users.noreply.github.com>
2022-09-23 13:07:18 -0500

    Delete p3-study-buddy-backend-main 2 directory

--- p3-study-buddy-backend-main 2/src/main/java/com/revature/studybuddy/repository/SetRepository.java
+++
@@ -1,9 +0,0 @@
-package com.revature.studybuddy.repository;
-
-import com.revature.studybuddy.entity.Set;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SetRepository extends JpaRepository<Set, Long> {
-}
